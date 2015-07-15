7750c96ebb6ca75b524427f47ea1ac7be64af54a
Ben Turner <ben.turner@go2mobi.com>
2015-07-14 20:20:31 -0700

    Use []string instead of sqlite database

--- requester.go
+++ requester.go
@@ -3,9 +3,10 @@ package main
 import (
 	"net/http"
 	"fmt"
-	"code.google.com/p/go-sqlite/go1/sqlite3"
 	"os"
 )
+
+var requests []string
 
 func main() {
 	os.Remove("database.db")
@@ -30,48 +31,23 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Bad token! Talk to Ben T.")
 		return
 	}
-	conn, connErr := sqlite3.Open("database.db")
-	if (connErr != nil) {
-		fmt.Print(connErr)
-		panic("Failed to open connection!")
-	}
-	defer conn.Close()
-	err := conn.Exec("INSERT INTO requests VALUES ('', '" + r.FormValue("text") + "')")
-	if (err != nil) {
-		fmt.Print(err)
-		panic("Failed to save")
-	}
+	requests = append(requests, r.FormValue("text"))
 	fmt.Fprintf(w, "Successfully requested %s!", r.FormValue("text"))
 	fmt.Println("request for " + r.FormValue("text") + " saved successfully")
 }
 
 func playerHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("View request")
-	conn, err := sqlite3.Open("database.db")
-	if (err != nil) {
-		panic("Failed to open connection!")
-	}
-	defer conn.Close()
-	sql := "SELECT url FROM requests"
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Fprintf(w, `{"requests":[`)
-	var first = true
-	for s, err := conn.Query(sql); err == nil; err = s.Next() {
-		var row string
-		s.Scan(&row)
-		fmt.Fprintf(w, `"%s"`, row)
-		if (!first) {
-			fmt.Fprintf(w, ", ")
-		}
-		first = false;
+	for i := 0; i < len(requests); i++ {
+		fmt.Fprintf(w, `"%s"%s`, slice[i], i == len(requests) ? "" : ", ")
 	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	requests = nil
+	
 	fmt.Fprintf(w, `], "playlist":[`)
 	fmt.Fprintf(w, `"XE-oMOEZ7Rc"`)
 	fmt.Fprintf(w, `]}`)
-
-	conn.Exec("DROP TABLE requests");
-	conn.Exec("VACUUM");
-	conn.Exec("CREATE TABLE requests(id INT AUTO_INCREMENT, url VARCHAR(128))")
 }
